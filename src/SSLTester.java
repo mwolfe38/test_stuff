8f7d7927a1d9adf55a1977e025e79b0fb3099379
Matt Wolfe <mwolfe@taser.com>
2011-05-03 18:27:11 -0700

    updates

--- src/SSLTester.java
+++ src/SSLTester.java
@@ -21,27 +21,20 @@ public class SSLTester {
 	public static final int DEFAULT_MAX_CONNECTIONS = 10;
 	public static final int DEFAULT_SOCKET_TIMEOUT = 10 * 1000;
 	public static final int DEFAULT_MAX_RETRIES = 5;
-	private static final String ENCODING = "UTF-8";
-	private static final String HEADER_ACCEPT_ENCODING = "Accept-Encoding";
-	private static final String ENCODING_GZIP = "gzip";
+	
 
-	private static int maxConnections = DEFAULT_MAX_CONNECTIONS;
-	private static int socketTimeout = DEFAULT_SOCKET_TIMEOUT;
-	private static DefaultHttpClient httpClient;
-	private static HttpContext httpContext;
 
 	public static void main(String[] args) throws ClientProtocolException,
 			IOException {
-		BasicHttpParams httpParams = new BasicHttpParams();
-				
+						
 		SchemeRegistry schemeRegistry = new SchemeRegistry();
 		schemeRegistry.register(new Scheme("http",80, PlainSocketFactory.getSocketFactory()));
 		schemeRegistry.register(new Scheme("https", 443, SSLSocketFactory.getSocketFactory()));
 		ThreadSafeClientConnManager cm = new ThreadSafeClientConnManager(schemeRegistry);
 
-		httpContext = new SyncBasicHttpContext(new BasicHttpContext());
-		httpClient = new DefaultHttpClient(cm, httpParams);
-		String url = "https://prod.evidence.com/index.aspx?class=UIX&proc=Login";
+		SyncBasicHttpContext httpContext = new SyncBasicHttpContext(new BasicHttpContext());
+		
+		String url = "https://mail.google.com/";
 		//url = "https://github.com";
 		HttpGet httpget = new HttpGet(url);
 		DefaultHttpClient httpclient = new DefaultHttpClient();
